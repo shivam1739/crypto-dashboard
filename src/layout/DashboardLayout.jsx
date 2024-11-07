a4f8acf4e89c920b7b66f38bbcc088b23870e55c
shivam1739 <patelshivam1739@gmail.com>
2024-11-07 13:54:13 +0530

    responsiveness updated

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -10,11 +10,11 @@ const DashboardLayout = ({ children }) => {
       <div className="col-span-1 dark:bg-white-lite">
         <DashboardHeader />
       </div>
-      <div className="grid gap-0 grid-cols-1 lg:grid-cols-10 col-span-1 h-full overflow-y-auto">
-        <div className="col-span-1 lg:col-span-7 self-start p-4 pt-0 dark:bg-white-lite">
+      <div className="lg:grid gap-0 grid-cols-1 lg:grid-cols-10 col-span-1 h-full overflow-y-auto ">
+        <div className="max-h-fit lg:col-span-7 lg:self-start p-4 pb-0 dark:bg-white-lite ">
           {children}
         </div>
-        <div className="col-span-1 lg:col-span-3 self-start lg:border-l-2 border-gray_lite">
+        <div className="col-span-1 lg:col-span-3 lg:self-start lg:border-l-2 border-gray_lite">
           <Suspense fallback={<div>Loading...</div>}>
             <RightInfoBar />
           </Suspense>
